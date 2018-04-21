affef3f6ef3dde07c1bf97bafeb620efd8b99ca4
georges <gderemusat@gmail.com>
2018-04-21 18:14:42 +0200

    test

--- app/Exceptions/AlreadyExistingProviderException.php
+++ app/Exceptions/AlreadyExistingProviderException.php
@@ -6,9 +6,11 @@ use Exception;
 
 class AlreadyExistingProviderException extends Exception
 {
-    //
     public function render()
     {
-        return redirect('/login')->with(['flash' => 'You are already signed up with another platform']);
+        return redirect('/login')->with('flash', [
+            'message' => 'You are already signed up with another platform',
+            'level' => 'warning'
+        ]);
     }
 }
