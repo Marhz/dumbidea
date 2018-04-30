c807e0fe4a6dc5dc9ebf0c6499a912b2d9b43c6b
georges <gderemusat@gmail.com>
2018-04-30 16:05:12 +0200

    mix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
 	<title>{{ config('app.name', 'Laravel') }}</title>
 
 	<!-- Styles -->
-	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+	<link href="{{ mix('css/app.css') }}" rel="stylesheet">
 	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous" async defer>
 	<script>
         window.App = {!! json_encode([
@@ -94,6 +94,6 @@
 	</div>
 
 	<!-- Scripts -->
-	<script src="{{ asset('js/app.js') }}"></script>
+	<script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
